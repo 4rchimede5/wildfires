866a82d5d28d7253d5c722ef0655af00b1abb221
4rchimede5 <116129185+4rchimede5@users.noreply.github.com>
2023-03-21 15:11:24 -0500

    Update 04.otsu_thresh.py

--- 04.otsu_thresh.py
+++ 04.otsu_thresh.py
@@ -2,7 +2,7 @@ import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv.imread('/home/zeus/Desktop/bomkumagua/1.png')
+img = cv.imread('')
 
 #global thresholding
 ret1,th1 = cv.threshold(img,127,255,cv.THRESH_BINARY)
